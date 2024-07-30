0dde95968a645bb7718e018bea727a535a4361da
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-30 11:32:09 +0100

    Add FirebaseAnalyticsClient, update analyticsFacadeProvider
    # Conflicts:
    #	lib/src/monitoring/analytics_facade.dart
    #	lib/src/monitoring/analytics_facade.g.dart
    
    # Conflicts:
    #	lib/src/monitoring/analytics_facade.g.dart
    
    # Conflicts:
    #	lib/src/monitoring/analytics_facade.g.dart

--- lib/src/monitoring/analytics_facade.g.dart
+++ lib/src/monitoring/analytics_facade.g.dart
@@ -49,4 +49,4 @@ final class AnalyticsFacadeProvider
   }
 }
 
-String _$analyticsFacadeHash() => r'42be3f71a863cfd3e4652bc1ca9059022b5402ed';
+String _$analyticsFacadeHash() => r'1dad4a58b2d8287648cf4d0649eedd0c1eb7335a';
